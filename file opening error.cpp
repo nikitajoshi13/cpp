b9335852bb4447e48a9796e97d5622592ac57196
nikitajoshi13 <34381368+nikitajoshi13@users.noreply.github.com>
2018-03-31 17:51:28 +0530

    Update file opening error.cpp

--- file opening error.cpp
+++ file opening error.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
 	ifstream fin;
-	fin.open("noname.txt");
+	fin.open("noname.txt");   #no such file existed in my system
 	if(!fin)
 	cout<<"error opening file";
 	exit(1);
